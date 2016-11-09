efbb5c586c11a33a100566794821085cf71b526b
Ryan Tang <ryantang@uchicago.edu>
2016-11-08 22:50:18 -0600

    updated e1/e2

--- e1.py
+++ e1.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-ELECTION_ID = {}
+id_list = {}
 url = "http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2015/office_id:1/stage:General"
 r  = requests.get(url)
 data = r.text
@@ -9,14 +9,14 @@ data = r.text
 soup = BeautifulSoup(data, "html.parser")
 rows = soup.find_all("tr")
 
-for row in rows:
-    id = str(row.get("id"))
-    begin = id.find("election-id-")
-    if (begin != -1):
-        election_id  = id[12:]
-        cell = row.find("td")
-        year = cell.string
-        ELECTION_ID[year] = election_id
-
-for k,v in sorted(ELECTION_ID.items(), reverse=True):
-    print (k,v)
+with open("ELECTION_ID", "w") as out:
+    for row in rows:
+        #print (row)
+        id = str(row.get("id"))
+        begin = id.find("election-id-")
+        if (begin != -1):
+            election_id  = id[12:]
+            cell = row.find("td")
+            year = cell.string
+            e = year + " " + election_id + "\n"
+            out.write(e)
